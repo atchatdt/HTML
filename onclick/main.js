4324263159e79eaa9fdfcb4fa383ba87f86e33b4
ThanhTuan <kisuvitinh@gmail.com>
2019-03-20 17:04:27 +0700

    onclick

--- onclick/main.js
+++ onclick/main.js
@@ -5,27 +5,36 @@ function render(){
 
 
 	var  htmlList = document.getElementById('todo');
-
+/*
 	var content= todoList.map(function(item){
 		return '<li>' + item + '</li>' + '<button onclick = deleteItem();> Xoa </button>';
 	});
-
-
 	htmlList.innerHTML = content.join('');
+	*/
+	var Content='';
+	for(var i=0;i< todoList.length;i++){
+		Content += '<li onclick="deleteItem(' + i + ')">' + todoList[i] + '</li>';
+	}
+	htmlList.innerHTML=Content;
+	
 
 }
-
 
 function addItem(){
 	var item =document.getElementById('item');
 	var newItem=item.value;
-	todoList.push(newItem);
-	render();
-	item.value='';
+	if(newItem)
+	{
+		todoList.push(newItem);
+		render();
+		item.value='';
+	}
+
 }
 
-function deleteItem(){
-	console.log('deleteItem');
+function deleteItem(index){
+	todoList.splice(index,1);
+	render();
 }
 render();
 
