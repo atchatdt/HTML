03eb5ba54753a7ccd3246444d86b2a575dc36bf6
ThanhTuan <kisuvitinh@gmail.com>
2019-03-20 22:38:26 +0700

    listener

--- eventlistenner/main.js
+++ eventlistenner/main.js
@@ -1,23 +1,17 @@
-
 var todoList =['Eat','Sleep'];
 
+var addBtn= document.getElementById('btn');
+//addBtn.onclick=addItem;
+addBtn.addEventListener('click',addItem);
+addBtn.addEventListener('click',function(){
+	console.log('anonymous');
+});
 function render(){
-
-
 	var  htmlList = document.getElementById('todo');
-/*
 	var content= todoList.map(function(item){
-		return '<li>' + item + '</li>' + '<button onclick = deleteItem();> Xoa </button>';
+		return '<li>' + item + '</li>' ;
 	});
 	htmlList.innerHTML = content.join('');
-	*/
-	var Content='';
-	for(var i=0;i< todoList.length;i++){
-		Content += '<li onclick="deleteItem(' + i + ')">' + todoList[i] + '</li>';
-	}
-	htmlList.innerHTML=Content;
-	
-
 }
 
 function addItem(){
@@ -30,11 +24,4 @@ function addItem(){
 		item.value='';
 	}
 }
-
-function deleteItem(index){
-	todoList.splice(index,1);
-	render();
-}
 render();
-
-
